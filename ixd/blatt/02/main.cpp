3f3a81b28265b61755cf200781f4095572770afc
x4heid <4heid@informatik.uni-hamburg.de>
2016-04-14 17:08:54 +0200

    changed .gitignore, added ignore for ixd/dami/gpu.pdfs

---
+++ ixd/blatt/02/main.cpp
@@ -0,0 +1,14 @@
+/*
+ * Interaktionsdesign, SoSe 2015
+ * Zettel 1, Aufgabe 2
+ * Autor(en): ??
+ */
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    std::cout << "Hello World!" << std::endl;
+    
+    return 0;
+}
